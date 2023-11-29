73ba25b5f9ef8c793aa8092121c40953c4766e3a
Prakash Poojar <126979638+PrakashCPoojar@users.noreply.github.com>
2023-11-29 23:37:15 +0530

    removed the unwanted code

--- server.js
+++ server.js
@@ -7,27 +7,22 @@ const pdfProcessor = require('./pdfProcessor');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files
 app.use(express.static(path.join(__dirname, 'my-web-app-client/build')));
 
-// Parse JSON requests
 app.use(express.json());
 
-// Handle chat API endpoint
 app.post('/api/chat', async (req, res) => {
   const { userInput } = req.body;
 
   try {
-    // Extract text from the PDF
     const pdfText = await pdfProcessor.extractTextFromPDF('sample.pdf');
     const sentences = pdfText.split('. ');
 
-    // Make a request to the OpenAI API
     const response = await axios.post(
       'https://api.openai.com/v1/engines/text-davinci-003/completions',
       {
         prompt: userInput,
-        max_tokens: 50, // Adjust as needed
+        max_tokens: 50,
       },
       {
         headers: {
@@ -39,7 +34,6 @@ app.post('/api/chat', async (req, res) => {
 
     const botResponse = response.data.choices[0].text.trim();
 
-    // Set the title dynamically based on the bot response
     const pageTitle = `My Web App - ${botResponse}`;
 
     res.json({ message: botResponse, title: pageTitle });
@@ -49,9 +43,7 @@ app.post('/api/chat', async (req, res) => {
   }
 });
 
-// Serve the main HTML file for any other routes
 app.get('*', (req, res) => {
-  // You can retrieve the title from the response sent in the '/api/chat' route
   const pageTitle = res.locals.title || 'My Web App';
 
   res.sendFile(path.join(__dirname, 'my-web-app-client/build', 'index.html'), {
@@ -59,7 +51,6 @@ app.get('*', (req, res) => {
   });
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
